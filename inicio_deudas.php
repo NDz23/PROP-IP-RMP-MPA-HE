0c6a841c0226ee8d9ddf1cbd5581afdef14215d0
NDz23 <diaznelson23@gmail.com>
2018-08-09 01:23:44 -0600

    Revert "Version 1.0 Revision 1"
    This reverts commit 272b9e3d9c8881891c36649dc969259cbab38d8a.

--- inicio_deudas.php
+++ inicio_deudas.php
@@ -8,7 +8,7 @@ if (isset($_GET['alerta'])) {
             echo '<div class="alert alert-danger"><strong>Error!</strong> Asegurese de que las cantidades ingresadas sean valores numéricos.</div>';
             break;
         case '3':
-            echo '<div class="alert alert-success"><strong>Éxito!</strong> Se insertó correctamente a la base de datos, para verificarlo, seleccione "Ver deudas pendientes" o "Ver deudas saldadas".</div>';
+            echo '<div class="alert alert-success"><strong>Éxito!</strong> Se insertó correctamente a la base de datos, para verificarlo, desplácese hacia abajo.</div>';
             break;
         case '10':
             echo '<div class="alert alert-danger"><strong>¡Error!</strong> Hay un error al conectar con  la base de datos.</div>';
@@ -17,17 +17,26 @@ if (isset($_GET['alerta'])) {
              break;
     }                
 }
-if (!isset($_GET['op'])) {//Se define el periodo para mostrar informes
 ?>
+<<<<<<< HEAD
     <div class="container">
         <form action="proceso_op_deudas.php" class="form-horizontal" method="post">    
             <h2>Deudas</h2>
             <select class="form-control" id="op" name="slcDeuda">
+=======
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 col-ms-12 col-sm-12 col-xs-12">
+            <p style="color:#ffffff">Seleccione una opción</p>
+            <select class="form-control" id="slcDeuda" name="slcDeuda">
+>>>>>>> parent of 272b9e3... Version 1.0 Revision 1
               <option>Seleccione</option>
               <option id="op" name="op" >Agregar nueva deuda</option>
               <option id="op" name="op" onclick="mostrarVerDeuda()">Ver deudas pendientes</option>
               <option id="op" name="op" onclick="mostrarDeudaSaldada()">Ver deudas saldadas</option>
             </select>
+<<<<<<< HEAD
             <div class="form-group">
                         <button type="submit" class="button">Ir</button>
             </div>
@@ -140,23 +149,128 @@ if (!isset($_GET['op'])) {//Se define el periodo para mostrar informes
                     </tr>
                   </thead>
                   <tbody>
-                <?php
-                    $res = $conexion->ejecutarConsulta($sql2);
-                    if(!$res){
-                        header('Location: inicio.php?pag=deudas&alerta=10');
-                        $conexion->cerrarConexion();
-                        exit();
-                    }
-                    while ($fila = $res->fetch_assoc()) {
-                        echo '<tr scope="row"><td class="desc">'.$fila['DESCRIPCION'].'</td><td class="desc">'.$fila['FECHA'].'</td><td class="desc">Lps. '. $fila['CANTIDAD'].'</td><td class="desc">Lps. '. $fila['CANT_PAGADA'].'</td><td class="desc">'. $fila['ACREEDOR'].'</td></tr>';
-                    }
-                ?>
-                  </tbody>
-                </table>
+=======
+        </div>
+    </div>
+</div>
+
+<div id="divCuerpo" name="divCuerpo">
+
+</div>
+
+<div class="container" id="divNuevaDeuda" name="divNuevaDeuda" style='display:none;'>
+    <form action="proceso_nuevo_deuda.php" class="form-horizontal" method="post">    
+            <h2>Tabule los datos de la deuda</h2>
+            
+            <div class="form-group">
+                    <label>Descripción de la deuda</label>
+                    <input class="text" id="descripcion" name="descripcion" placeholder="Ingrese la descripción de la deuda aquí." rows="0"></input>
             </div>
-        <?php
-        }
+            
+            <div class="form-group">
+                <label>Fecha</label>
+                <input class="datepicker" id="fecha" name="fecha" type="text" value=
+>>>>>>> parent of 272b9e3... Version 1.0 Revision 1
+                <?php
+                    $hoy = getdate();
+                    echo '"'.$hoy['year'].'-';
+                    if ($hoy['mon']<10) {
+                        echo "0";
+                    }
+                    echo $hoy['mon'].'-';
+                    if ($hoy['mday']<10) {
+                        echo "0";
+                    }
+                    echo $hoy['mday'].'"';
+                ?> readonly>
+            </div>
+                
+            <div class="form-group">
+                    <label>Cantidad adeudada</label>
+                    <input class="text" id="cantidad" name="cantidad" placeholder="Ingrese la cantidad adeudada aquí." rows="0"></input>
+            </div>
+            <div class="form-group">
+                    <label>Acreedor</label>
+                    <input class="text" id="acreedor" name="acreedor" placeholder="Ingrese el nombre del acreedor aquí." rows="0"></input>
+            </div>
+            <div class="form-group">
+                    <label>Cantidad pagada</label>
+                    <input class="text" id="cantidad_paga" name="cantidad_paga" placeholder="Ingrese la cantidad pagada aquí." rows="0"></input>
+            </div>
+
+            <div class="form-group">
+                    <button type="submit" class="button">Terminar</button>
+            </div>
+    </form>
+</div>
+
+<?php
+include_once "./class/class_conexion.php";
+$conexion = new Conexion();
+$sql = 'SELECT * FROM `tbl_deudas` WHERE `CANTIDAD`>`CANT_PAGADA`';
+$sql2 = 'SELECT * FROM `tbl_deudas` WHERE `CANTIDAD`-`CANT_PAGADA`=0';
+$res = $conexion->ejecutarConsulta($sql);
+if(!$res){
+    header('Location: inicio.php?pag=deudas&error=10');
     $conexion->cerrarConexion();
-    }
+    exit();
 }
 ?>
+<<<<<<< HEAD
+=======
+<div class="container" id="divVerDeuda" name="divVerDeuda" style='display:none;'>  
+    <h2>Deudas pendientes</h2>
+    <table class="table">
+	  <thead>
+	    <tr>
+	      <th class="desc">Descripción</th>
+	      <th class="desc">Fecha</th>
+	      <th class="desc">Cantidad</th>
+	      <th class="desc">Cantidad pagada</th>
+	      <th class="desc">Acreedor</th>
+	      <th class="desc">Editar</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	  	<?php
+        while ($fila = $res->fetch_assoc()) {
+            echo '<tr scope="row"><form action="proceso_pago_deuda.php" method="post"><td class="desc">'.$fila['DESCRIPCION'].'</td><td class="desc">'.$fila['FECHA'].'</td><td class="desc">'. $fila['CANTIDAD'].'</td><td class="desc"><input class="text" id="cant" name="cant" placeholder="'. $fila['CANT_PAGADA'].'" rows="0"></input></td><td class="desc">'. $fila['ACREEDOR'].'</td><td class="desc"><input type="hidden" name="id" value="'.$fila['ID_DEUDA'].'"/><button type="submit" class="button">Editar</button></td></form></tr>';
+        }
+    	?>
+	  </tbody>
+	</table>
+</div>
+<div class="container" id="divDeudaSaldada" name="divDeudaSaldada" style='display:none;'>  
+    <h2>Deudas saldadas</h2>
+    <table class="table">
+	  <thead>
+	    <tr>
+	      <th class="desc">Descripción</th>
+	      <th class="desc">Fecha</th>
+	      <th class="desc">Cantidad</th>
+	      <th class="desc">Cantidad pagada</th>
+	      <th class="desc">Acreedor</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+    <?php
+        $res = $conexion->ejecutarConsulta($sql2);
+        if(!$res){
+            header('Location: inicio.php?pag=deudas&error=10');
+            $conexion->cerrarConexion();
+            exit();
+        }
+        while ($fila = $res->fetch_assoc()) {
+            echo '<tr scope="row"><td class="desc">'.$fila['DESCRIPCION'].'</td><td class="desc">'.$fila['FECHA'].'</td><td class="desc">Lps. '. $fila['CANTIDAD'].'</td><td class="desc">Lps. '. $fila['CANT_PAGADA'].'</td><td class="desc">'. $fila['ACREEDOR'].'</td></tr>';
+        }
+    ?>
+      </tbody>
+	</table>
+</div>
+<footer class="footer footer-copyright text-center py-3">© 2018 Por: Nelson Díaz y Fabricio Murillo</footer>
+  <script src='js/jquery.min.js'></script>
+  <script src="js/index.js"></script>
+  <script src="js/deuda.js"></script>
+</body>
+</html>
+>>>>>>> parent of 272b9e3... Version 1.0 Revision 1
